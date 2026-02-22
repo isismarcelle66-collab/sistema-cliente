fd96f9c8cabed23eb2a4a9694967272b9b9fee24
Isis Marcelle <isis.marcelle66@gmail.com>
2026-02-22 08:47:31 -0300

    Revert debug

--- app/database.py
+++ app/database.py
@@ -3,13 +3,11 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "database.db")
-print("CAMINHO DO BANCO:", DB_PATH)
 
 def get_connection():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
-
 
 def init_db():
     conn = get_connection()
