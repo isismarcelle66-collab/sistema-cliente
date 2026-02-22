087cd76d30ed7e46a50aae00fdc974a95bf03878
Isis Marcelle <isis.marcelle66@gmail.com>
2026-02-22 08:19:57 -0300

    Adicionando rota POST criar Lead

--- app/routes/leads.py
+++ app/routes/leads.py
@@ -1,26 +1,14 @@
-from fastapi import APIRouter
-from app.database import get_connection
-
-router = APIRouter()
-
-@router.get("/api/leads")
-def listar_leads():
+@router.post("/api/leads")
+def criar_lead(nome: str, email: str, telefone: str):
     conn = get_connection()
     cursor = conn.cursor()
 
     cursor.execute(
-        "SELECT id, nome, email, telefone, status FROM leads"
+        "INSERT INTO leads (nome, email, telefone, status) VALUES (?, ?, ?, ?)",
+        (nome, email, telefone, "novo")
     )
-    leads = cursor.fetchall()
+
+    conn.commit()
     conn.close()
 
-    return [
-        {
-            "id": l[0],
-            "nome": l[1],
-            "email": l[2],
-            "telefone": l[3],
-            "status": l[4],
-        }
-        for l in leads
-    ]
+    return {"message": "Lead criado com sucesso"}
