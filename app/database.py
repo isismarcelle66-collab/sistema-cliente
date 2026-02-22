d004c25057c34a0a362965014ee72df22a1bc48b
Isis Marcelle <isis.marcelle66@gmail.com>
2026-02-22 08:36:40 -0300

    Debug caminho banco

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "database.db")
-
+print("CAMINHO DO BANCO:", DB_PATH)
 
 def get_connection():
     conn = sqlite3.connect(DB_PATH)
